724f6f92ad80805a2daa8829917e37486ab80150
EnigmaDX <derryckdxd@gmail.com>
2017-12-06 04:36:21 +0000

    add to firebase db complete

--- app/src/main/java/com/example/derrickdowuona/roadwatch/RegisterActivity.java
+++ app/src/main/java/com/example/derrickdowuona/roadwatch/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -66,32 +67,25 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    ///USER
-    public void writeNewUser(String name, String email, int phone, int age) {
-        Users user = new Users(name, email, phone, age);
-        Log.w(TAG, "USER DETAILS===================" + user.toString());
-        Toast.makeText(RegisterActivity.this, user.toString(), Toast.LENGTH_SHORT).show();
-        myRef.setValue(user);
-    }
     //END onCreate
 
     public void verifyAndSendData()
     {
         final String usernameStr = username.getText().toString();
         final String emailStr = email.getText().toString();
-        final int phoneInt = Integer.parseInt(phone.getText().toString());
+        final String phoneStr = phone.getText().toString();
         final String ageStr = age.getText().toString();
 
-//        int phoneInt = 0;
+        int phoneInt = 0;
         int ageInt = 0;
-//        try{
-//            phoneInt = Integer.parseInt(phoneInt);
-//            Log.i("",phoneInt+" is a number");
-//        }catch(NumberFormatException ex){
-//            Log.w(TAG, "EXCEPTIONNNNNNNNNNN");
-//            Log.i("",phoneInt+" is NOT a number");
-//            ex.printStackTrace();
-//        }
+        try{
+            phoneInt = Integer.parseInt(phoneStr);
+            Log.i("",phoneInt+" is a number");
+        }catch(NumberFormatException ex){
+            Log.w(TAG, "EXCEPTIONNNNNNNNNNN");
+            Log.i("",phoneInt+" is NOT a number");
+            ex.printStackTrace();
+        }
 
         try{
              ageInt = Integer.parseInt(ageStr);
@@ -100,22 +94,43 @@ public class RegisterActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
 
+        if(!TextUtils.isEmpty(usernameStr) || !TextUtils.isEmpty(emailStr))
+        {
+            Intent intent = new Intent(RegisterActivity.this, HomePage.class);
+            Bundle extras = new Bundle();
+            extras.putString("USERNAME",usernameStr);
+            extras.putString("EMAIL",emailStr);
+            extras.putInt("PHONE",phoneInt);
+            extras.putInt("AGE",ageInt);
 
-        Intent intent = new Intent(RegisterActivity.this, HomePage.class);
-        Bundle extras = new Bundle();
-        extras.putString("USERNAME",usernameStr);
-        extras.putString("EMAIL",emailStr);
-        extras.putInt("PHONE",phoneInt);
-        extras.putInt("AGE",ageInt);
+            intent.putExtras(extras);
+            startActivity(intent);
 
-        Log.w(TAG, "Numbersssssss: " + phoneInt + " AGE:: " + ageInt);
-        writeNewUser(usernameStr, emailStr, phoneInt, ageInt);
+            Log.w(TAG, "Numbersssssss: " + phoneInt + " AGE:: " + ageInt);
+            writeNewUser(usernameStr, emailStr, phoneInt, ageInt);
+        }
+        else
+        {
+            Toast.makeText(RegisterActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        }
 
-        //call verify method for phone
-//        startPhoneNumberVerification(phoneStr);
 
-        intent.putExtras(extras);
-        startActivity(intent);
+
+    }
+
+    ///USER
+    public void writeNewUser(String name, String email, int phone, int age)
+    {
+        Users user = new Users(name, email, phone, age);
+
+        String uid = myRef.push().getKey();
+
+        myRef.child(uid).setValue(user);
+
+        Log.w(TAG, "USER DETAILS===================" + user.toString());
+
+        Toast.makeText(RegisterActivity.this, user.toString(), Toast.LENGTH_SHORT).show();
+
     }
 
 
